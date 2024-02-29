34cc8bbc9c4ec6ea7dc0f1c8c9cb0265c72bba76
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-29 17:59:08 +0100

    wrote functions index, show, create, update

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -5,22 +5,65 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+//Models
+use App\Models\Comic;
+
 class ComicController extends Controller
 {
+    /*
+        R - READ
+    */
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $comics = Comic::all();
+
+        return view('comics.index', compact('comics'));
     }
 
+    /**
+     * Display the specified resource.
+     */
+    // Versione 1: con dependency injection
+    public function show(Comic $comic)
+    {
+        return view('comics.show', compact('comic'));
+    }
+
+    // Versione 2: con find e if per controllare se è stato trovato quello che cercavamo
+    // public function show(string $id)
+    // {
+    //     $pasta = Pasta::find($id);
+
+    //     if ($pasta == null) {
+    //         // Vai in 404
+    //         abort(404);
+    //     }
+
+    //     return view('pastas.show', compact('pasta'));
+    // }
+
+    // Versione 3: con findOrFail (che, nel caso in cui non trovasse niente che corrisponde a quella query, dà 404)
+    // public function show(string $id)
+    // {
+    //     $pasta = Pasta::findOrFail($id);
+
+    //     return view('pastas.show', compact('pasta'));
+    // }
+    /* -------------- FINE READ -------------- */
+
+
+    /*
+        C - CREATE
+    */
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        return view('comics.create');
     }
 
     /**
@@ -28,17 +71,28 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $singleComicData = $request->all();
+
+        $comic = new Comic();
+        $comic->title = $singleComicData['title'];
+        $comic->description = $singleComicData['description'];
+        $comic->src = $singleComicData['thumb'];
+        $comic->price = $singleComicData['price'];
+        $comic->series = $singleComicData['series'];
+        $comic->sale_date = $singleComicData['sale_date'];
+        $comic->type = $singleComicData['type'];
+        $comic->artists = implode(", ", $singleComicData['artists']);
+        $comic->writers = implode(", ", $singleComicData['writers']);   
+        $comic->save();
+
+        return redirect()->route('comic.show', ['comic' => $comic->id]);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+    /* -------------- FINE CREATE -------------- */
 
+    /*
+        U - UPDATE
+    */
     /**
      * Show the form for editing the specified resource.
      */
@@ -55,6 +109,11 @@ class ComicController extends Controller
         //
     }
 
+    /* -------------- FINE UPDATE -------------- */
+
+    /*
+        D - DELETE
+    */
     /**
      * Remove the specified resource from storage.
      */
@@ -62,4 +121,5 @@ class ComicController extends Controller
     {
         //
     }
+    /* -------------- FINE DELETE -------------- */
 }
