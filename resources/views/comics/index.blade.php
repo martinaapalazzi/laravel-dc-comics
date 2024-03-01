2628ea7b76dd69081c947d933300ff71a41aec75
martinaapalazzi <martinaapalazzi@gmail.com>
2024-03-01 15:01:58 +0100

    Created small form in html for function destroy

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -18,16 +18,18 @@
 
         <table class="table">
             <thead>
-                <tr>
+                <tr class="text-center">
                     <th scope="col">#</th>
                     <th scope="col">Poster:</th>
                     <th scope="col">Title:</th>
                     <th scope="col">Price:</th>
                     <th scope="col">Release date:</th>
                     <th scope="col">Check it out:</th>
+                    <th scope="col">Edit:</th>
+                    <th scope="col">Delete:</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody class="text-center">
                 @foreach ($comics as $comic)
                     <tr>
                         <th scope="row">{{ $comic->id }}</th>
@@ -42,8 +44,25 @@
                         <td>{{ $comic->sale_date }}</td>
                         <td>
                             <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="btn btn-primary">
-                                Check out
+                                Check 
                             </a>
+                        </td>
+                        <td>
+                            <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="btn btn-warning">
+                                Edit
+                            </a>
+                        </td>
+                        <td>
+                            <form
+                                action="{{ route('comics.destroy', ['comic' => $comic->id]) }}"
+                                onsubmit="return confirm('Are you sure you want to delete this comic?')"
+                                method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">
+                                    Delete
+                                </button> 
+                            </from>
                         </td>
                     </tr>
                 @endforeach
